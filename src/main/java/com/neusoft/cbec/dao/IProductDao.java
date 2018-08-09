c4cd6b235a1152e46f5594cd0642fa710e6a8e36
admin <admin@C6502030>
2018-08-09 16:26:16 +0800

    IProductDaoMapper和Dao层完善

--- src/main/java/com/neusoft/cbec/dao/IProductDao.java
+++ src/main/java/com/neusoft/cbec/dao/IProductDao.java
@@ -1,5 +1,8 @@
 package com.neusoft.cbec.dao;
 
+
+import java.util.List;
+
 import com.neusoft.cbec.model.ProductModel;
 
 /**
@@ -8,5 +11,12 @@ import com.neusoft.cbec.model.ProductModel;
  */
 public interface IProductDao {
 	//新建商品
-	public void  create (ProductModel pd) throws Exception;
+	public void create (ProductModel pd) throws Exception;
+	//修改
+	public void update (ProductModel pd) throws Exception;
+	//删除
+	public void delete (ProductModel pd) throws Exception;
+	//查询
+	public List<ProductModel> selectListByAll() throws Exception;
+	
 }

a52b6d99c66a587ee0bbb7f75411e86872f6e403
Administrator <Administrator@DESKTOP-E0FMK2Q>
2018-08-10 14:57:37 +0800

    orderitem getlistbyall

--- src/main/java/com/neusoft/cbec/controller/OrderItemController.java
+++ src/main/java/com/neusoft/cbec/controller/OrderItemController.java
@@ -1,9 +1,12 @@
 package com.neusoft.cbec.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.neusoft.cbec.model.ManufacturerModel;
 import com.neusoft.cbec.model.OrderItemModel;
 import com.nuesoft.cbec.service.IOrderItemService;
 
@@ -19,5 +22,10 @@ public void setOrderitemService(IOrderItemService orderitemService) {
   public String  add(OrderItemModel em)throws Exception{
 	  orderitemService.add(em);
 	  return "ok";
+  }
+  @RequestMapping("/getListByAll")
+  public List<OrderItemModel> getListByAll()throws Exception{
+		List<OrderItemModel> list = orderitemService.getListByAll();
+		return list;
   }
 } 

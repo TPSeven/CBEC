41e935e788709edb622074dbdc1982ee08206216
link <link@DESKTOP-7AC0HO5>
2018-08-12 17:35:09 +0800

    页面显示商品信息

---
+++ src/main/webapp/product/main.js
@@ -0,0 +1,27 @@
+/**
+ * 商品管理主JS程序
+ */
+//页面载入成功事件
+$(document).ready(function(){
+	//请求取得部门列表的REST API
+	$.getJSON("product/list/all.mvc",function(productList){
+		alert("start");
+		var lines="";
+		for(var i=0;i<productList.length;i++){
+	lines=lines+"<tr><td>"+productList[i].pro_id+"</td>" +
+					"<td>"+productList[i].pro_name+"</td>" +
+					"<td>"+productList[i].pro_price+"</td>" +
+					"<td>"+productList[i].pro_weight+"</td>" +
+					"<td>"+productList[i].pro_count+"</td>" +
+					"<td>"+productList[i].pro_kinds_id+"</td>" +
+					"<td>"+productList[i].pro_state_id+"</td>" +
+					"<td>"+productList[i].brand_id+"</td>" +
+					"<td>"+productList[i].pro_photos_id+"</td>" +
+					"<td>"+productList[i].pro_desc+"</td></tr>"
+					
+		}
+		$("table#productListTable tbody").html(lines);
+	});
+	alert("ok");
+	
+});

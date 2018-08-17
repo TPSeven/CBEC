1adc15e83d876d15f06ff11693f05dd75f3d7912
Administrator <Administrator@DESKTOP-E0FMK2Q>
2018-08-17 15:16:35 +0800

    orderitem增加检索条件

--- src/main/java/com/neusoft/cbec/controller/OrderItemController.java
+++ src/main/java/com/neusoft/cbec/controller/OrderItemController.java
@@ -1,8 +1,10 @@
 package com.neusoft.cbec.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,4 +56,21 @@ public void setOrderitemService(IOrderItemService orderitemService) {
 	public OrderItemModel getOrderById(@RequestParam(required=true)int id) throws Exception{
 		return orderitemService.getOrderItemById(id);
 	}
+  
+  @RequestMapping(value="listbycondition")
+	public List<OrderItemModel> getListByCondition(@RequestParam(required=false,defaultValue="0") int order_id, @RequestParam(required=false,defaultValue="0") int man_id, @RequestParam(required=false) @DateTimeFormat(pattern="yyyy-MM-dd") Date startDate, @RequestParam(required=false) @DateTimeFormat(pattern="yyyy-MM-dd")  Date endDate,@RequestParam(required=false,defaultValue="") String man_name) throws Exception {
+	if(man_name!=null&&man_name.trim().length()>0){
+		man_name="%"+man_name+"%";
+	}
+		return orderitemService.getListByCondition(order_id, man_id, startDate, endDate,man_name);
+	}
+
+  @RequestMapping(value="listbyconditionwithpage")
+	public List<OrderItemModel> getListByConditionWithPage(int order_id, int man_id, Date startDate, Date endDate,String man_name, int rows,
+			int page)
+			throws Exception {
+	
+		return orderitemService.getListByConditionWithPage(order_id, man_id, endDate, startDate, man_name,rows*(page-1)+1,rows*page);
+	}
+  
 } 

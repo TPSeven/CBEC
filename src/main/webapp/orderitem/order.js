59e45e4a47ac0eaf35c779f14dfac73ce206c440
Administrator <Administrator@DESKTOP-E0FMK2Q>
2018-08-22 23:08:18 +0800

    orderitem页面进一步完善

--- src/main/webapp/orderitem/order.js
+++ src/main/webapp/orderitem/order.js
@@ -1,7 +1,11 @@
 /**
  * 
  */
-
+jQuery.ajaxSettings.traditional=true;
+$.ajaxSetup({
+	traditional:false,
+	cache:false //close AJAX cache
+  });
 
 $(document).ready(function(){
 	var o_id=0;
@@ -44,7 +48,7 @@ $(document).ready(function(){
               multiselect:false,
               onSelectRow:function(id){
              	o_id=id;
-             	 alert(o_id);
+             	
               }
               
     });
@@ -59,16 +63,21 @@ function getParamAndReloadGrid(){
 	 if(endDate!=null){
 		 datas.endDate=endDate;
 	 }
+	 //清除累积的参数
+	 var postData = $('table#orderitemGrid').jqGrid("getGridParam","postData");
+	 $.each(postData, function(k,v){
+		 delete postData[k];
+	 })
+	 //设置新的参数
 	 $("table#orderitemGrid").jqGrid("setGridParam",{postData:datas}).trigger("reloadGrid");
 	
 }
 
 //取得订单编号更改事件
 $("input#order_id").on("change",function(){
-	order_id1=$("input#order_id").val();
-	
-   getParamAndReloadGrid();
-
+	order_id=$("input#order_id").val();
+	  getParamAndReloadGrid();
+   
 });
 //取得姓名更改事件
      $("input#man_name").on("change",function(){
@@ -104,5 +113,259 @@ $("input#order_id").on("change",function(){
      //  alert(state);
      
      });
+  
+     //增加订单按钮点击事件处理
+     $("a#orderlistAddLink").on("click",function(){
+    	
+    	  
+    	$("div#OrderItemDialog").load("orderitem/add.html", function(){
+     	//嵌入增加页面
+    		$("div#OrderItemDialog").dialog({
+     		title:"增加新订单",
+    	width:900,
+    	heigth:650   
+    	});
+      //使用JQuery validate对订单进行数据验证
+    		
+    		$("form#addorderitemForm").validate({
+    			rules:{
+    			order_id:{
+        				required:true,
+        				//rangelength:[1,10],
+        				rangelengthforOA:[1,10],
+    		            remote:{
+    		            	url:"orderitem/checkIDCanBeUsed.mvc",
+    		                type:"POST",
+    		                data:{
+    		                        order_id:function(){
+    		                		return  $("input[name='order_id']").val();
+    		                	 }
+    		                }
+    		            }
+    		    },
+    			pro_id:{
+    				required:true,
+    				rangelength:[1,5]
+    			},
+    			pro_id_count:{
+    				required:true,
+    				digits:true,
+    				range:[50,10000]
+    			},
+    			man_id:{
+    				required:true,
+    				rangelength:[1,5]
+    			},
+    			order_date:{
+    				required:true
+    			},
+    			seller_id:{
+    				required:true,
+    				rangelength:[1,5]
+    			},
+    			state:{
+    				required:true
+    			}
+    			},
+    			messaeges:{
+    				order_id:{
+        				required:"订单编号为空",
+        			//	rangelength:"订单编号要在1到10为长度",
+        				remote:"订单编号已经存在"
+        			},
+    				pro_id:{
+        				required:"产品编号为空",
+        				rangelength:"产品编号要在1到5为长度"
+        			},
+        			pro_id_count:{
+        				required:"产品数量为空",
+        				digits:"产品数量必须是整数",
+        			    range:"产品数量要在50到10000"
+        			},
+        			man_id:{
+        				required:"制造商编号为空",
+        				rangelength:"制造商编号要在1到5为长度"
+        			},
+        			order_date:{
+        				required:"生日为空"
+        			},
+        			seller_id:{
+        				required:"借卖方编号为空",
+        				rangelength:"借卖方编号要在1到5为长度"
+        			},
+        			state:{
+        				required:"订单状态为空"
+        			}
+    			}
+    		});
+    		
+    		
+    		
+    		
+    	//拦截订单增加表单提交
+		$("form#addorderitemForm").ajaxForm(function(result){
+			alert(result.message);
+			
+			  getParamAndReloadGrid(); //重新载入订单列表，并刷新Grid显示。
+		
+			$("div#OrderItemDialog").dialog("close"); //关闭弹出Dialog
+		
+		});
+		//定义取消连接点击事件处理
+		$("a#OrderItemCancelButton").on("click",function(){
+			$("div#OrderItemDialog").dialog("close"); //关闭弹出Dialog
+		
+		});
+    	});
+     });
+   //修改订单按钮点击事件处理
+    $("a#orderlistModifyLink").on("click",function(){
+    	$("div#OrderItemDialog").load("orderitem/modify.html",function(){
+    	
+    		if(o_id==0){
+    		alert("请选择要修改的订单！");
+    		}else
+    			{
+    			//嵌入修改页面
+        		$("div#OrderItemDialog").dialog({
+            		title:"修改订单",
+            	width:900,
+            	heigth:650   
+            	});
+    			 $.getJSON("orderitem/getOrderById.mvc",{id:o_id},function(resultData){
+ 					$("input[name='order_id']").val(resultData.order_id);
+    					$("input[name='pro_id']").val(resultData.pro_id);
+    					$("input[name='pro_id_count']").val(resultData.pro_id_count);
+    					$("input[name='man_id']").val(resultData.man_id);
+    					$("input[name='seller_id']").val(resultData.seller_id);
+    					$("input[name='state']").val(resultData.state);
+    					$("input[name='order_date']").val(resultData.order_date);
+    				 });
+    			}
+    		$("form#modifyorderitemForm").validate({
+    			rules:{
+    			pro_id:{
+    				required:true,
+    				rangelength:[1,5]
+    			},
+    			pro_id_count:{
+    				required:true,
+    				digits:true,
+    				range:[50,10000]
+    			},
+    			man_id:{
+    				required:true,
+    				rangelength:[1,5]
+    			},
+    			order_date:{
+    				required:true
+    			},
+    			seller_id:{
+    				required:true,
+    				rangelength:[1,5]
+    			},
+    			state:{
+    				required:true
+    			}
+    			},
+    			messaeges:{
+    				pro_id:{
+        				required:"产品编号为空",
+        				rangelength:"产品编号要在1到5为长度"
+        			},
+        			pro_id_count:{
+        				required:"产品数量为空",
+        				digits:"产品数量必须是整数",
+        			    range:"产品数量要在50到10000"
+        			},
+        			man_id:{
+        				required:"制造商编号为空",
+        				rangelength:"制造商编号要在1到5为长度"
+        			},
+        			order_date:{
+        				required:"生日为空"
+        			},
+        			seller_id:{
+        				required:"借卖方编号为空",
+        				rangelength:"借卖方编号要在1到5为长度"
+        			},
+        			state:{
+        				required:"订单状态为空"
+        			}
+    			}
+    		});
+    		$("form#modifyorderitemForm").ajaxForm(function(result){
+    			alert(result.message);
+    			
+    			  getParamAndReloadGrid(); //重新载入订单列表，并刷新Grid显示。
+    		
+    			$("div#OrderItemDialog").dialog("close"); //关闭弹出Dialog
+    		
+    		});
+    		//定义取消连接点击事件处理
+    		$("a#OrderItemCancelButton").on("click",function(){
+    			$("div#OrderItemDialog").dialog("close"); //关闭弹出Dialog
+    		
+    		});
+    	});
+    	
+     });
+    //删除订单按钮事件处理
+    $("a#orderlistDeleteLink").on("click",function(){
+    
+    		if(o_id==0){
+    			alert("请选择要删除的订单!");
+    			
+    		}else{ 
+    			var confirmResult=confirem("确认要删除选择的订单吗?");
+    			if(confirmResult){
+    			    $.post("orderitem/delete.mvc",{order_id:o_id},function(result){
+    			  if(result=="ok"){
+    				   alert("删除订单成功");
+    			  }else{
+    				  alert("删除订单失败");
+    			  }
+    			});
+    			    } 
+    			getParamAndReloadGrid(); 
+    			}
+    
+    			
+    });
+    //查看订单俺就事件处理
+    $("a#orderlistViewLink").on("click",function(){
+        
+		if(o_id==0){
+			alert("请选择要查看的订单!");
+			
+		}else{ 
+		
+			$("div#OrderItemDialog").load("orderitem/view.html",function(){
+				$("div#OrderItemDialog").dialog({
+            		title:"查看订单",
+            	width:900,
+            	heigth:650   
+            	});
+		
+			    	 $.getJSON("orderitem/getOrderById.mvc",{id:o_id},function(resultData){
+			    		    $("span#order_id").html(resultData.order_id);
+			    		    $("span#order_date").html(resultData.order_date);
+			 				$("span#pro_id").html(resultData.pro_id);
+			 				$("span#pro_id_count").html(resultData.pro_id_count);
+			 				$("span#man_id").html(resultData.man_id);
+			 				$("span#seller_id").html(resultData.seller_id);
+			 				$("span#state").html(resultData.state);
+			 			 
+			    	 });
+			    	   $("button#OrderItemCancelButton").on("click",function(){
+			    			$("div#OrderItemDialog").dialog("close"); //关闭弹出Dialog
+			    		
+			    });
+	    		});
+			    } 
+	
+			
 
+			
+});
 });
